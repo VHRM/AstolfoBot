eeb31d2f78d05217b6cc1797d4128b69cbef9d17
vhrm <vhrm@cin.ufpe.br>
2020-01-25 12:34:26 -0300

    Funcoes novas

--- index.js
+++ index.js
@@ -36,19 +36,47 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 
     if(command === "favoritos" || command === "favs"){
-            const usuario = "<@"+message.author.id+">";
-            messageEmbed.setTitle("Favoritos:");
-            messageEmbed.setDescription("Favoritos de " + usuario);
-            message.channel.send(messageEmbed);
-            db.each('SELECT * FROM favoritos WHERE user = ?', [message.author.id], (err, row) => {
-                if(err)
-                    throw(err);
-                    messageEmbed.setTitle(row.nome);
-                    messageEmbed.setDescription(row.link);
-                    message.channel.send(messageEmbed);
+        const usuario = "<@"+message.author.id+">";
+        messageEmbed.setTitle("Favoritos:");
+        messageEmbed.setDescription("Favoritos de " + usuario);
+        message.channel.send(messageEmbed);
+        db.each('SELECT * FROM favoritos WHERE user = ?', [message.author.id], (err, row) => {
+            if(err)
+                throw(err);
+                messageEmbed.setTitle(row.nome);
+                messageEmbed.setDescription(row.link);
+                message.channel.send(messageEmbed);
+        });
+    }
+
+    if(command === "adicionar" || command === "add"){
+        const usuario = "<@"+message.author.id+">";
+        const mangaURL = args.pop();
+        const mangaNome = args.join(' ');
+        db.run('INSERT INTO favoritos (nome, user, link) VALUES( ?, ?, ?)', [mangaNome,message.author.id,mangaURL], err => {
+            if (err)
+                throw(err);
+            else{
+                messageEmbed.setTitle("Adicionado");
+                messageEmbed.setDescription("O manga: " + mangaNome + " foi adicionado à lista de favoritos de " + usuario);
+                message.channel.send(messageEmbed);
+            }
+        });
+    }
+
+    if(command === "remover"|| command === "rmv"){
+        const usuario = "<@"+message.author.id+">";
+        const mangaNome = args.join(' ');
+        db.run('DELETE FROM favoritos WHERE nome = ?', [mangaNome], err =>{
+            if (err)
+            throw(err);
+            else{
+                messageEmbed.setTitle("Removido");
+                messageEmbed.setDescription("O manga: " + mangaNome + " foi removido da lista de favoritos de " + usuario);
+                message.channel.send(messageEmbed);
+            }
         })
     }
-}
-);
+});
 
 client.login(token);
